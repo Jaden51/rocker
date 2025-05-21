231cf0e4ce54c308eceae31f24b50408573a35db
Jaden Hums <jadenhums51@gmail.com>
2025-05-21 16:00:02 -0400

    adding cli commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use clap::Parser;
+use clap::Subcommand;
+
+#[derive(Parser)]
+#[command(name = "rocker")]
+#[command(about = "Docker in Rust", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Run { cmd: String },
+    Images,
+    Ps,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Run { cmd } => {
+            run_container(cmd);
+        }
+        Commands::Images => {
+            list_images();
+        }
+        Commands::Ps => {
+            show_containers();
+        }
+    }
+}
+
+fn run_container(cmd: &String) {
+    println!("command: {}", cmd)
+}
+
+fn list_images() {
+    println!("Listing images");
+}
+
+fn show_containers() {
+    println!("Showing containers");
 }
