08ec7f50832a667edbaa1a71a0a506e6716f1257
Jaden Hums <jadenhums51@gmail.com>
2025-05-22 23:06:46 +0000

    creating new container process

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
-use clap::Parser;
-use clap::Subcommand;
+use std::ffi::CString;
+use std::fs::create_dir_all;
+
+use clap::{Parser, Subcommand};
+use nix::mount::{MsFlags, mount, umount};
+use nix::sched::{CloneFlags, unshare};
+use nix::sys::wait::waitpid;
+use nix::unistd::{ForkResult, chroot, execvp, fork, sethostname, write};
 
 #[derive(Parser)]
 #[command(name = "rocker")]
@@ -11,18 +17,17 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    Run { cmd: String },
+    Run { cmd_args: Vec<String> },
     Images,
     Ps,
-    
 }
 
 fn main() {
     let cli = Cli::parse();
 
     match &cli.command {
-        Commands::Run { cmd } => {
-            run_container(cmd);
+        Commands::Run { cmd_args } => {
+            run_container(cmd_args);
         }
         Commands::Images => {
             list_images();
@@ -33,8 +38,78 @@ fn main() {
     }
 }
 
-fn run_container(cmd: &String) {
-    println!("command: {}", cmd)
+fn run_container(cmd_args: &Vec<String>) {
+    println!("command: {:?}", cmd_args);
+
+    unshare(CloneFlags::CLONE_NEWUTS | CloneFlags::CLONE_NEWPID | CloneFlags::CLONE_NEWNS)
+        .expect("Failed to unshare namespaces");
+
+    match unsafe { fork() } {
+        Ok(ForkResult::Parent { child, .. }) => {
+            println!("Parent: created container process with PID: {}", child);
+
+            waitpid(child, None).expect("Failed to wait for child");
+
+            let proc_path = "./rootfs/proc";
+            // if std::path::Path::new(proc_path).exists() {
+            //     if let Err(e) = umount(proc_path) {
+            //         eprintln!("Warning: Failed to unmount {}: {}", proc_path, e);
+            //     }
+            // }
+        }
+        Ok(ForkResult::Child) => {
+            sethostname("default").expect("Failed to set hostname");
+
+            create_dir_all("./rootfs/proc").unwrap();
+            match mount(
+                Some("proc"),
+                "./rootfs/proc",
+                Some("proc"),
+                MsFlags::empty(),
+                None::<&str>,
+            ) {
+                Ok(_) => {
+                    write(std::io::stdout(), "Mounted /proc successfully\n".as_bytes()).ok();
+                }
+                Err(e) => {
+                    let msg = format!("Mount failed: {}\n", e);
+                    write(std::io::stderr(), msg.as_bytes()).ok();
+                }
+            }
+
+            match chroot("./rootfs") {
+                Ok(_) => {
+                    write(std::io::stdout(), "chroot succeeded\n".as_bytes()).ok();
+                }
+                Err(e) => {
+                    let msg = format!("chroot failed: {}\n", e);
+                    write(std::io::stderr(), msg.as_bytes()).ok();
+                }
+            }
+
+            match std::env::set_current_dir("/") {
+                Ok(_) => {
+                    write(std::io::stdout(), "setdir succeeded\n".as_bytes()).ok();
+                }
+                Err(e) => {
+                    let msg = format!("setdir failed: {}\n", e);
+                    write(std::io::stderr(), msg.as_bytes()).ok();
+                }
+            }
+            let cmd = CString::new(cmd_args[0].as_str()).unwrap();
+            let args: Vec<CString> = cmd_args
+                .iter()
+                .map(|s| CString::new(s.as_str()).unwrap())
+                .collect();
+
+            execvp(&cmd, &args).expect("Failed to execute command");
+
+            println!("child done");
+        }
+        Err(_) => println!("Fork failed"),
+    }
+
+    println!("done run");
 }
 
 fn list_images() {
